83d8d461feed671ee8e012125298ae232e547eee
Albina Peposhi <codergirl@pop-os.localdomain>
2024-07-06 12:02:25 +0200

    wip

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 15:53:53 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/05/15 01:07:59 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/07/05 19:57:33 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,48 +20,57 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-typedef struct s_texture
-{
-	char	*path;
-	int		width;
-	int		height;
-}			t_texture;
+// typedef struct s_texture
+// {
+// 	char	*path;
+// 	int		width;
+// 	int		height;
+// }			t_texture;
 
-typedef struct s_player
-{
-	double	x;
-	double	y;
-	double	dir_x;
-	double	dir_y;
-	double	plane_x;
-	double	plane_y;
-}				t_player;
+// typedef struct s_player
+// {
+// 	double	x;
+// 	double	y;
+// 	double	dir_x;
+// 	double	dir_y;
+// 	double	plane_x;
+// 	double	plane_y;
+// }				t_player;
 
-typedef struct s_settings
-{
-	int		sc_width;
-	int		sc_height;
-	double	sp_move;
-	double	sp_rotation;
-}				t_settings;
+// typedef struct s_settings
+// {
+// 	int		sc_width;
+// 	int		sc_height;
+// 	double	sp_move;
+// 	double	sp_rotation;
+// }				t_settings;
 
-typedef struct s_game
+// typedef struct s_game
+// {
+// 	char		*map_data;
+// 	char		**map;
+// 	t_texture	north;
+// 	t_texture	south;
+// 	t_texture	east;
+// 	t_texture	west;
+// 	int			floor[3];
+// 	int			ceiling[3];
+// 	t_player	player;
+// 	t_settings	settings;
+// }				t_game;
+
+typedef	struct s_data
 {
-	char		*map_data;
 	char		**map;
-	t_texture	north;
-	t_texture	south;
-	t_texture	east;
-	t_texture	west;
-	int			floor[3];
-	int			ceiling[3];
-	t_player	player;
-	t_settings	settings;
-}				t_game;
+	int			p_x;
+	int			p_y;
+	int			w_map;
+	int			h_map;
+}				t_data;
 
 // validator functions
 int	ft_validate_args(int argc, char **argv);
-
+int	f
 // parser functions
 
 // utility functions
