85aec5496f74288974e1acc959160c60423fc876
JorFik <138692812+JorFik@users.noreply.github.com>
2024-07-12 21:29:38 +0200

    feature (main for playground)

--- src/main.c
+++ src/main.c
@@ -3,28 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 15:52:21 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/07/06 20:06:19 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/07/12 19:18:46 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/cub3d.h"
+#include "cub3d.h"
 
-int main(int argc, char **argv)
+int	map_reader(int argc, char **argv, mlx_t *window)
 {
-	t_game game;
+	int		fd;
+	char	*line;
 
-	// init data structure
-	// validate arguments
-	if (ft_validate_args(argc, argv))
-		return (1);
-	// read into map
-	if (ft_read_map(game, char *map))
-		return (1);
-	// validate the map
-	// initialize game
-	// set up hookers nd shit
-	//         HOOKS
+	if (argc != 2)
+		return (ft_print_err("Invalid number of arguments\n", 1));
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		return (ft_print_err("Invalid file\n", 1));
+	line = get_next_line(fd);
+	while (line)
+	{
+		line = get_next_line(fd);
+		printf("%s\n", line);
+		free(line);
+	}
+	free(line);
+	close(fd);
+	window = mlx_init(100, 100, "Cub3D", true);
+	return (0);
+}
+
+
+int	main(int argc, char **argv)
+{
+	mlx_t	window;
+
+	map_reader(argc, argv, &window);
 }
