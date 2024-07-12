c7c050e6023ce7c942ef482e52f4762217cf5c98
JorFik <138692812+JorFik@users.noreply.github.com>
2024-07-12 19:04:17 +0200

    Fix (norm)

--- src/error_handling.c
+++ src/error_handling.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   error_handling.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 23:31:56 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/05/14 23:35:47 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/07/12 14:34:27 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/cub3d.c"
+#include "cub3d.h"
 
 int	ft_print_err(char *message, int r_value)
 {
