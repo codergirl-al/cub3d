575935cb61a275846f278c3b5aa39f84af846784
JorFik <138692812+JorFik@users.noreply.github.com>
2024-07-31 15:59:17 +0200

    Fix (loop condition)
    now taking into account index starts with 0

--- src/execution/cast_rays.c
+++ src/execution/cast_rays.c
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 18:19:53 by JFikents          #+#    #+#             */
-/*   Updated: 2024/07/30 16:31:47 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/07/30 19:37:37 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	init_ray_images(mlx_t *window, t_ray_data *ray)
 	i = 0;
 	ray->img = mlx_new_image(window, 1, 1);
 	mlx_put_pixel(ray->img, 0, 0, 0xFF00FFFF);
-	while (i <= RAY_COUNT)
+	while (i < RAY_COUNT)
 	{
 		mlx_image_to_window(window, ray->img, 0, 0);
 		i++;
@@ -70,7 +70,7 @@ t_ray_data	*cast_rays(t_loop_data *data)
 	i = 0;
 	angle = data->player->angle - ((FOV * PI / 180) / 2);
 	angle = adjust_angle(angle, 0);
-	while (i <= RAY_COUNT)
+	while (i < RAY_COUNT)
 	{
 		wall_coords = get_closer_wall_to_player(data, angle, &rays_data[i]);
 		ray->instances[i].x = wall_coords[X];
