0e5108231ce0f180a6bfdd4979535d26b5a58cdb
Albina Peposhi <codegirl.al@gmail.com>
2024-08-12 01:12:15 +0200

    Raw data handler functionality

---
+++ src/parser/raw_data_handler.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raw_data_handler.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 04:42:27 by apeposhi          #+#    #+#             */
+/*   Updated: 2024/08/11 19:26:11 by apeposhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+int ft_handle_raw_data(t_data *playground)
+{
+	int		i;
+	int		j;
+	char	**data;
+
+	i = 0;
+	data = ft_split(playground->raw_data, '\n');
+	while (i < 6)
+	{
+		if (data[i][0] == 'F')
+            ft_handle_input(playground, data[i], 'F');
+		else if (data[i][0] == 'C')
+			ft_handle_input(playground, data[i], 'C');
+		else if (data [i][0] == 'N')
+		{
+			j = -1;
+			while (++j < 4)
+			{
+				playground->textures = ft_strjoin(playground->textures, data[i++]);
+				playground->textures = ft_strjoin(playground->textures, "\n");
+			}
+			ft_handle_textures(playground);
+			i += 4;
+		}
+		else
+			return (ft_handle_invalid(playground));
+    }
+	free(data);
+	return (1);
+}
