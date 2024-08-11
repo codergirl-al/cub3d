73b6ca1308ab953fcc3db39aff03e8d477fbf25c
Albina Peposhi <codergirl@pop-os.localdomain>
2024-08-11 21:24:59 +0200

    Handling errors - like a pro

--- src/error_handling.c
+++ src/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 23:31:56 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/07/17 16:31:49 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/08/11 21:01:14 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	ft_print_err(char *message, int r_value)
 {
-	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(message, 2);
-	return (r_value);
+	exit (r_value);
+}
+
+int ft_handle_invalid(t_data *playground)
+{
+  ft_free_data(playground);
+  return (ft_print_err("Error\n", 0));
 }
