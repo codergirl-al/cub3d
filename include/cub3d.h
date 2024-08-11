b23835f9d647b80f3ae22172c775a1f8f5ae5dd1
Albina Peposhi <codegirl.al@gmail.com>
2024-08-12 01:13:49 +0200

    Cleanup and making it work

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 15:53:53 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/07/22 15:23:02 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/08/09 22:25:14 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,93 +20,56 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-// typedef struct s_texture
-// {
-// 	char	*path;
-// 	int		width;
-// 	int		height;
-// }			t_texture;
-
-// typedef struct s_player
-// {
-// 	double	x;
-// 	double	y;
-// 	double	dir_x;
-// 	double	dir_y;
-// 	double	plane_x;
-// 	double	plane_y;
-// }				t_player;
-
-// typedef struct s_settings
-// {
-// 	int		sc_width;
-// 	int		sc_height;
-// 	double	sp_move;
-// 	double	sp_rotation;
-// }				t_settings;
-
-// typedef struct s_game
-// {
-// 	char		*map_data;
-// 	char		**map;
-// 	t_texture	north;
-// 	t_texture	south;
-// 	t_texture	east;
-// 	t_texture	west;
-// 	int			floor[3];
-// 	int			ceiling[3];
-// 	t_player	player;
-// 	t_settings	settings;
-// }				t_game;
-
-typedef struct	s_game
+typedef struct s_texture
 {
-	char		*map_data;
-	char		**map_2d;
-	// t_texture	n_txt;
-	// write all textures for each textures
-}				t_game;
+	char	*north;
+	char	*south;
+	char	*east;
+	char	*west;
+}			t_texture;
 
 typedef struct s_data
 {
-	char	**map_2d;
-	int		map_rows;
-	int		map_cols;
-	int		player_x;
-	int		player_y;
-	char	player_d;
-	int		map_width;
-	int		map_height;
-	char	*floor_data;
-	char	*ceiling_data;
-	int		fd;
-	int		index;
-	char	*current_line;
-	char	*texture_key;
-	char	*map_data;
-	char	*textures;
-	char	**square_map;
-	char	**rgb_values;
-	// t_txtr *textures;
+	char		**map_2d;
+	char		*raw_data;
+	int			map_rows;
+	int			map_cols;
+	int			player_x;
+	int			player_y;
+	char		player_d;
+	int			map_width;
+	int			map_height;
+	char		*floor_data;
+	char		*ceiling_data;
+	int			fd;
+	int			index;
+	char		*current_line;
+	char		*texture_key;
+	char		*map_data;
+	char		*textures;
+	char		**square_map;
+	char		**rgb_values;
+	t_texture	textur;
 }	t_data;
 
 // validator functions
 int	ft_validate_args(int argc, char **argv);
 
 // parser functions
-// t_data	ft_parse(t_data *playground, int argc, char **argv);
-t_data	ft_parse(int argc, char **argv);
+void	ft_parse(int argc, char **argv, t_data *playground);
+int		ft_handle_raw_data(t_data *playground);
 int		ft_handle_textures(t_data *playground);
 int		ft_handle_map(t_data *playground);
-int		ft_handle_input(t_data *playground, char flag);
+int		ft_handle_input(t_data *playground, char *data, char flag);
 // parser utilities
-int	ft_check_ending(char *filename);
+int		ft_check_ending(char *filename);
 
 // utility functions
-int	ft_print_err(char *message, int r_value);
+int		ft_print_err(char *message, int r_value);
 
 // cleanup 
 
 void	ft_free_data(t_data *gd);
+int		ft_handle_invalid(t_data *playground);
 
 #endif
