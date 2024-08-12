f912519ceb7af3e2ba04a844e9b12a74d127c46a
Albina Peposhi <codegirl.al@gmail.com>
2024-08-12 02:46:04 +0200

    Added parsing for floor and ceiling

--- src/parser/f_c_handler.c
+++ src/parser/f_c_handler.c
@@ -6,31 +6,53 @@
 /*   By: apeposhi <apeposhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 15:23:19 by apeposhi          #+#    #+#             */
-/*   Updated: 2024/08/12 01:07:52 by apeposhi         ###   ########.fr       */
+/*   Updated: 2024/08/12 02:45:32 by apeposhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static int	ft_count_chstr()
+size_t ft_strcountchr(char *str, char c)
 {
-	
+  int i;
+  size_t counter;
+
+  i = -1;
+  counter = 0;
+  while (str[++i]) {
+    if (str[i] == c)
+      counter++;
+  }
+  return (counter);
 }
 
-static int	ft_parse_color(t_data *playground)
+static int	ft_parse_color(char *data, int colors[3])
 {
 	char	**rgb;
+	char	**prsd;	
 
-	if (ft_count_chstr())
+	prsd = ft_split(data, ' ');
+	if (ft_strcountchr(prsd[1], ',') != 2)
+		return (1);
+	rgb = ft_split(prsd[1], ',');
+	if (ft_arrlen(rgb) != 3)
+		return (ft_arrfree(rgb), 1);
+	colors[0] = ft_digit_check(rgb[0]);
+	colors[1] = ft_digit_check(rgb[1]);
+	colors[2] = ft_digit_check(rgb[2]);
+	return (ft_arrfree(rgb), ft_arrfree(prsd), 0);
 }
 
+// to add: ERROR HANDLING
 int ft_handle_input(t_data *playground, char *data, char flag)
 {
-	playground->ceiling_data = "bkabala";
-	data[1] = 'f';
 	if (flag == 'C')
- 		printf("============== %c: %s\n", flag, playground->ceiling_data);
-	else
-		printf("============== %c: %s\n", flag, playground->floor_data);
+	{
+		ft_parse_color(data, playground->c_colors);
+	}
+	else if (flag == 'F')
+	{
+		ft_parse_color(data, playground->f_colors);
+	}
 	return (1);
 }
